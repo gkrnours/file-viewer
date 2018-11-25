16a1e830445e7c5804250dc33a0adce8a1c3e955
ludovic coues <gkr@jellycopter.net>
2018-11-25 20:49:40 +0100

    Add basic pagination (next & previous link)

--- src/files/static/files/main.js
+++ src/files/static/files/main.js
@@ -1,18 +1,33 @@
 var BASE_URL = window.location
 var TABLE = null;
 var PAGINATION = null;
+var NEXT_LINK = null;
+var PREV_LINK = null;
 var MODAL = document.getElementById("fileDetails");
+var PER_PAGE = 3;
 
 document.addEventListener("DOMContentLoaded", start);
 
+// initialize the page
 function start() {
-	TABLE = document.getElementById("content");
-	PAGINATION = document.getElementById("page");
+	TABLE = document.getElementById('content');
+	PAGINATION = document.getElementById('page');
+	NEXT_LINK = PAGINATION.querySelector('[data-role="paginate-next"]');
+	PREV_LINK = PAGINATION.querySelector('[data-role="paginate-previous"]');
 	fetch_page();
 
-	TABLE.addEventListener("click", on_open_file, true);
+	TABLE.addEventListener('click', on_open_file, true);
+	PAGINATION.addEventListener('click', on_change_page, true);
 }
 
+// Event Listener, when changing page
+function on_change_page() {
+	event.preventDefault();
+	// TODO handle real page and not just next/previous link
+	fetch_page(event.target.href);
+}
+
+// Event Listener, when opening the details of a file
 function on_open_file() {
 	if (event.target.dataset.action == "open") {
 		var row = event.target.closest("tr");
@@ -33,6 +48,7 @@ function on_open_file() {
 	}
 }
 
+// List of function, pretty-fying a file name for display
 var formatter = {
 	"text": function (str) { return str; },
 	"date": function (timestamp) {
@@ -44,10 +60,23 @@ var formatter = {
 	},
 };
 
+// Handle ajax, success and error
 function fetch_page(page) {
 	page = page || 0;
+	var url = '';
+	parsed_page = parseInt(page);
+	if (isNaN(parsed_page)) {
+		// We can get an url instead of a page number
+		url = page;
+	}
+	else {
+		url = BASE_URL + 'api/files/?limit='+PER_PAGE;
+		if (page) {
+			url += '&offset=' + PER_PAGE * parsed_page;
+		}
+	}
 	var xhr = new XMLHttpRequest();
-	xhr.open('GET', BASE_URL + 'api/files/', true);
+	xhr.open('GET', url, true);
 	xhr.send();
 
 	xhr.onload = function() {
@@ -62,11 +91,28 @@ function fetch_page(page) {
 		var data = JSON.parse(this.responseText);
 		console.log(data)
 
+		// Pagination
 		if (data.next || data.previous) {
-			PAGINATION.style = "";
+			PAGINATION.style = '';
+			// Previous & Next link
+			if (data.previous) {
+				PREV_LINK.href = data.previous;
+				PREV_LINK.parentElement.classList.remove('disabled');
+			} else {
+				PREV_LINK.parentElement.classList.add('disabled');
+			}
+			if (data.next) {
+				NEXT_LINK.href = data.next;
+				NEXT_LINK.parentElement.classList.remove('disabled');
+			} else {
+				NEXT_LINK.parentElement.classList.add('disabled');
+			}
+		} else {
+			PAGINATION.style = 'display:none;';
 		}
 
 		var tpl = TABLE.tHead.rows[0];
+		TABLE.tBodies[0].innerHTML = '';
 		for (var i = 0; i < data.results.length; ++i) {
 			var row = document.createElement("tr");
 			var file = data.results[i];
